b7befd56914918f8a53dff80a28fb65fc37e5e61
ABEDNEGO.WAMBUA <ABEDNEGO.WAMBUA>
2023-05-25 22:35:09 +0300

    slugging tests

--- tests/ArticleTest.php
+++ tests/ArticleTest.php
@@ -4,10 +4,26 @@ use PHPUnit\Framework\TestCase;
 
 class ArticleTest extends TestCase
 {
+    protected $article;
 
     public function testTitleIsEmptyByDefault()
     {
-        $article = new App\Article;
-        $this->assertEmpty($article->title);
+        $this->assertEmpty($this->article->title);
+    }
+
+    public function testSlugIsEmptyWithNoTitle()
+    {
+        $this->assertSame($this->article->getSlug(), "");
+    }
+
+    public function testSlugHasSpaceReplacedUnderscores()
+    {
+        $this->article->title = "An example article";
+        $this->assertEquals($this->article->getSlug(), 'An_example_article');
+    }
+
+    protected function setUp(): void
+    {
+        $this->article = new App\Article;
     }
 }
